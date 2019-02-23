0b2bcd9157a934a8e2b1e430e75e4d351dd50ce7
Emmanuel Semutenga <emzsema@gmail.com>
2019-02-23 22:37:52 +0300

    Finished User registration

--- src/django_project/settings.py
+++ src/django_project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
